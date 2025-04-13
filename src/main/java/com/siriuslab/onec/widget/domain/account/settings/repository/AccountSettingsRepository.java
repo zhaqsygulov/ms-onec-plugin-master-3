46c3fb3dfae2d7c7ffbdfa5ce1cbe72916db8f72
zhaqsygulov <zhaqsygulov@gmail.com>
2025-04-14 01:42:55 +0500

    Update AccountSettingsRepository.java

--- src/main/java/com/siriuslab/onec/widget/domain/account/settings/repository/AccountSettingsRepository.java
+++ src/main/java/com/siriuslab/onec/widget/domain/account/settings/repository/AccountSettingsRepository.java
@@ -3,6 +3,9 @@ package com.siriuslab.onec.widget.domain.account.settings.repository;
 import com.siriuslab.onec.widget.domain.account.settings.entity.AccountSettingsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.siriuslab.onec.widget.domain.account.token.entity.AccountEntity;
+import java.util.Optional;
+
 
 @Repository
 public interface AccountSettingsRepository extends JpaRepository<AccountSettingsEntity, Long> {
