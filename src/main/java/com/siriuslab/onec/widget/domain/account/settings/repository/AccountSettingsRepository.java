a31e639199e726f138ea1d74303101ec5202ea3a
zhaqsygulov <zhaqsygulov@gmail.com>
2025-04-14 01:51:55 +0500

    Update AccountSettingsRepository.java

--- src/main/java/com/siriuslab/onec/widget/domain/account/settings/repository/AccountSettingsRepository.java
+++ src/main/java/com/siriuslab/onec/widget/domain/account/settings/repository/AccountSettingsRepository.java
@@ -5,11 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.siriuslab.onec.widget.domain.account.token.entity.AccountEntity;
 import java.util.Optional;
+import org.springframework.web.multipart.MultipartFile;
+
 
 
 @Repository
 public interface AccountSettingsRepository extends JpaRepository<AccountSettingsEntity, Long> {
     Optional<AccountSettingsEntity> findByAccount(AccountEntity account);
 }
-
-
