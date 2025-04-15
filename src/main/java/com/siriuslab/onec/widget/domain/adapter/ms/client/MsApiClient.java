b4950a8812fc069f02c10e99fe6de6926524ffaf
zhaqsygulov <zhaqsygulov@gmail.com>
2025-04-15 17:58:30 +0500

    Update MsApiClient.java

--- src/main/java/com/siriuslab/onec/widget/domain/adapter/ms/client/MsApiClient.java
+++ src/main/java/com/siriuslab/onec/widget/domain/adapter/ms/client/MsApiClient.java
@@ -7,11 +7,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "msApiClient", url = "https://apps-api.moysklad.ru/api/vendor/1.0", configuration = FeignConfig.class)
+@FeignClient(
+    name = "msApiClient",
+    url = "https://apps-api.moysklad.ru/api/vendor/1.0",
+    configuration = FeignConfig.class
+)
 public interface MsApiClient {
-    @PostMapping(value = "/context/{context}")
+
+    @PostMapping("/context/{context}")
     GetEmployeeContextResponse getContext(
-            @RequestHeader("Authorization") String authorization,
-            @PathVariable String context
+        @RequestHeader("Authorization") String authorization,
+        @PathVariable("context") String context
     );
 }
