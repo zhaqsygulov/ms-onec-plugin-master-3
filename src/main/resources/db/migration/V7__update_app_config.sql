e33dae068f42e265356b74e54e2f758d80934f82
zhaqsygulov <zhaqsygulov@gmail.com>
2025-06-12 22:14:19 +0500

    Delete src/main/resources/db/migration/V7__update_app_config.sql

--- src/main/resources/db/migration/V7__update_app_config.sql
+++
@@ -1,7 +0,0 @@
-UPDATE app_config
-SET value ='QYWpy8D0LW6jGDP4x1niNyPYhYK0hv9DHXJLpAA9lSetqp9iIHobpooUKxwbC9s2bGbLvYmsK8WqK29RANQJRAHb01ubvDgVz3iyymwXVfGtSzpBTJ4Uuvk4GmOm1mem'
-WHERE key = 'jwt.secret';
-
-UPDATE app_config
-SET value = 'siriuslab1ckz.siriuslab'
-WHERE key = 'appUid';
