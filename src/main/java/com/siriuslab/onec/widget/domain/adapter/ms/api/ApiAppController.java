bb5579e8f48c5ef57a1525a732bc21bbc3f6ac3b
zhaqsygulov <zhaqsygulov@gmail.com>
2025-06-12 19:51:10 +0500

    Update ApiAppController.java

--- src/main/java/com/siriuslab/onec/widget/domain/adapter/ms/api/ApiAppController.java
+++ src/main/java/com/siriuslab/onec/widget/domain/adapter/ms/api/ApiAppController.java
@@ -1,5 +1,6 @@
 package com.siriuslab.onec.widget.domain.adapter.ms.api;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.siriuslab.onec.widget.app.component.JwtGenerator;
 import com.siriuslab.onec.widget.domain.account.token.entity.AccountEntity;
 import com.siriuslab.onec.widget.domain.account.token.service.AccountService;
@@ -10,7 +11,10 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
@@ -23,23 +27,24 @@ public class ApiAppController {
     private final AccountService accountService;
 
     @Operation(security = @SecurityRequirement(name = "basicAuth"))
-    @CrossOrigin(origins = "*") // или указать фронт: https://opt-order-frontend-9m7d.vercel.app
-    @RequestMapping(
-        value = "context/{contextKey}/employee",
-        method = {RequestMethod.GET, RequestMethod.OPTIONS}
-    )
+    @GetMapping("context/{contextKey}/employee")
     public ResponseEntity<GetEmployeeContextResponse> getContext(@PathVariable String contextKey) {
         log.info("Received contextKey: {}", contextKey);
-
+        // TODO move business logic into service
         String token = jwtGenerator.generateToken();
         jwtGenerator.verifyToken(token);
-
-        GetEmployeeContextResponse response = msApiService.getContext(token, contextKey);
+        GetEmployeeContextResponse response;
+        try {
+            response = msApiService.getContext(token, contextKey);
+        } catch (JsonProcessingException e) {
+            log.error("msApiService.getContext error by token : {}  context : {} error : {}",
+                    token, contextKey, e.getMessage());
+            throw new IllegalArgumentException(e);
+        }
 
         AccountEntity accountEntity = accountService.findByAccountId(response.getAccountId());
         response.setToken(accountEntity.getAccessToken());
         response.setSettings(accountEntity.getSettings());
-
         return ResponseEntity.ok(response);
     }
 }
