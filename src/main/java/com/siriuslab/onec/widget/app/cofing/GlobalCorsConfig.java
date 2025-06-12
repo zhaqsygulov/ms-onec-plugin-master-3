630f4cda51a64175e9a7fa7311473747beab9a79
zhaqsygulov <zhaqsygulov@gmail.com>
2025-06-12 17:46:54 +0500

    Create GlobalCorsConfig.java

---
+++ src/main/java/com/siriuslab/onec/widget/app/cofing/GlobalCorsConfig.java
@@ -0,0 +1,18 @@
+package com.siriuslab.onec.widget.app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class GlobalCorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*") // или явно указать фронт
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
